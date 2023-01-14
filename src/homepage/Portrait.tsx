6bb6e3981d8ef75244ab5ef8d8c221344619ccad
Fré Dumazy <dumazy@gmail.com>
2023-01-14 12:56:15 +0100

    Add portrait

---
+++ src/homepage/Portrait.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+import * as styles from "./Portrait.module.scss"
+
+
+
+const Portrait =  () => {
+
+    return (
+        <div className={styles.portrait}>
+            <StaticImage className={styles.bw} src="../images/portrait_bw.jpg" alt="Portrait Begüm Göktas" />
+            <StaticImage className={styles.color} src="../images/portrait_color.jpg" alt="Portrait Begüm Göktas" />
+        </div>
+    );
+}
+
+export default Portrait;
